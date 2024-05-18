7432847cb5d9f8e4fd4536479bf584f03a961788
alexquar <alexanderquarrie@gmail.com>
2024-05-18 01:55:17 -0400

    working map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,60 +1,40 @@
 import React from 'react'
-import {GoogleMap, useJsApiLoader} from '@react-google-maps/api'
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-}
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-}
+import { useState } from 'react';
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 export default function Home() {
-  // const initMap = () => {
-  //   const centerMap = { lat: 40.7128, lng: -74.0060}
-  //   const map = new google.maps.Map(document.getElementById("map"), {
-  //     center: centerMap,
-  //     zoom: 10,
-  //   });
-  // }
-  const {isLoaded} = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyAoD_LbQQXbvMnByd0fzqzweDXOjOvjylc'
-  })
-  const [map, setMap] = React.useState(null)
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-    setMap(map)
-  }, [])
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-    const handleClick = async () => {
-      const res = await fetch('/api/events', {
-        method:'POST',
-        body : JSON.stringify({
-          //insert thing here
-        }),
-    })
-
-    const json = await res.json()
-
-    if(!res.ok){
-console.log('good')
-    } 
-    else{
-      console.log('error')
-    }
-  }
-
+  const position = { lat: 43.4723, lng: -80.5449};
+  const [open, setOpen] = useState(false);
   return (
     
-    <div>
-          {isLoaded && <GoogleMap mapContainerClassName={containerStyle} center={center} zoom={10} onLoad={onLoad}></GoogleMap>}
+    <div className='flex justify-center flex-col mt-5'>
+       <APIProvider apiKey={'AIzaSyAoD_LbQQXbvMnByd0fzqzweDXOjOvjylc'} className='m-5'>
+      <div style={{ height: "75dvh", width: "100%" }}>
+        <Map defaultZoom={15} defaultCenter={position} mapId={'2e0f38f7239851a2 '}>
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"grey"}
+              borderColor={"green"}
+              glyphColor={"purple"}
+            />
+          </AdvancedMarker>
+
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>I'm in Waterloo</p>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
             <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
               <button style={{ marginRight: '10px' }}>Button 1</button>
-              <button onClick={handleClick} style={{ marginRight: '10px' }}>Button 2</button>
+              <button style={{ marginRight: '10px' }}>Button 2</button>
               <button>Button 3</button>
             </div>
             </div>
