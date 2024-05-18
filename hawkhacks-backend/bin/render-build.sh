b865959edad3d79567b7fc7d49893ee7648e3a84
JaneBabiakA <janefrances369@gmail.com>
2024-05-18 17:15:35 -0400

    New password stuff

--- hawkhacks-backend/bin/render-build.sh
+++ hawkhacks-backend/bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 #bundle exec rails assets:precompile
 #bundle exec rails assets:clean
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
