d77eddbee45241dc4c741c87218faf0310b3492d
JaneBabiakA <janefrances369@gmail.com>
2024-05-18 17:24:11 -0400

    Get rid of db drop

--- hawkhacks-backend/bin/render-build.sh
+++ hawkhacks-backend/bin/render-build.sh
@@ -5,8 +5,4 @@ set -o errexit
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle install
-#bundle exec rails assets:precompile
-#bundle exec rails assets:clean
-bundle exec rails db:drop
-bundle exec rails db:create
 bundle exec rails db:migrate
