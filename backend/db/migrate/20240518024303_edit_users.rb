5e35085167c23564d3a0941c093e61fcb9c3eda8
JaneBabiakA <janefrances369@gmail.com>
2024-05-18 15:34:03 -0400

    Deleted old backend

--- backend/db/migrate/20240518024303_edit_users.rb
+++
@@ -1,7 +0,0 @@
-class EditUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_reference :events, :user
-  end
-end
